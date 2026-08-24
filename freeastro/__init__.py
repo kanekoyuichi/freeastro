925ef09a69ad5767255f0380da0f60fc4977d963
Yuichi Kaneko <kaneko.yuichi@gmail.com>
2026-08-24 20:02:59 +0900

    Bump version to v0.2.1

--- freeastro/__init__.py
+++ freeastro/__init__.py
@@ -1,5 +1,5 @@
 from .subject import AstrologicalSubject
 from .models import Planet, House, Aspect, ChartData
 
-__version__ = "0.2.0"
+__version__ = "0.2.1"
 __all__ = ["AstrologicalSubject", "Planet", "House", "Aspect", "ChartData"]
